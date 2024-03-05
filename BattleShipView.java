257fafaa9e85d7c99f5f8a293b31c7d0373e2dc3
SpencerPresley <spencerpresley96@gmail.com>
2024-03-05 00:46:32 -0500

    almost done fixing dragndrop

--- BattleShipView.java
+++ BattleShipView.java
@@ -3,17 +3,17 @@ import java.awt.event.*;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 import java.awt.image.BufferedImage;
-
+import java.util.List;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.Image;
 
 public class BattleShipView{
 
     // currently being used for testing
     private static Game game = new Game();
     private static JButton[][] coordinateGrid;
-
+    private static MyPanel dragAndDrop;
 
     public static void main(String[] args) {
         coordinateGrid= new JButton[10][10];
@@ -32,6 +32,10 @@ public class BattleShipView{
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Ships Placement Confirmed");
+                List<Integer> shipCoordinates = placeShips();
+                for (int i = 0; i < shipCoordinates.size(); i++) {
+                    System.out.println(shipCoordinates.get(i));
+                }
             }
         });
 
@@ -54,7 +58,20 @@ public class BattleShipView{
         backgroundPanel.setLayout(new BorderLayout()); // Set the layout of the backgroundPanel to BorderLayout
         backgroundPanel.add(placementButtonPanel, BorderLayout.SOUTH);
 
-        MyPanel dragAndDrop = new MyPanel();
+        // Initialize the dragAndDrop panel with a specific size
+        dragAndDrop = new MyPanel();
+        dragAndDrop.setPreferredSize(new Dimension(1000, 1000)); // Set preferred size
+        
+        // Create a container panel with GridBagLayout for more control
+        JPanel containerPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.CENTER; // Center the dragAndDrop panel within the container
+        
+        // Add the dragAndDrop panel to the container panel
+        containerPanel.add(dragAndDrop, gbc);
+        
+        // Add the container panel to the backgroundPanel at the center
+        backgroundPanel.add(containerPanel, BorderLayout.CENTER);
 
         JButton battleship = new JButton("Rotate Battleship");
         battleship.addActionListener(new ActionListener() {
@@ -96,8 +113,8 @@ public class BattleShipView{
         buttonPanel.add(destroyer);
         backgroundPanel.add(buttonPanel, BorderLayout.NORTH);
 
-        
-        backgroundPanel.add(dragAndDrop, BorderLayout.CENTER);
+        backgroundPanel.add(containerPanel, BorderLayout.CENTER);
+        //backgroundPanel.add(dragAndDrop, BorderLayout.CENTER);
         frame.add(backgroundPanel);
         
         frame.pack();
@@ -106,6 +123,8 @@ public class BattleShipView{
         frame.setLocationRelativeTo(null); // centers the window
         frame.setResizable(false);
         frame.setVisible(true);
+
+
 
         // Action listener for play button. Takes from main menu to game screen.
         randButton.addActionListener(new ActionListener() {
@@ -159,6 +178,148 @@ public class BattleShipView{
             }
         });
     }
+
+
+    private static List<Integer> shipCoordinates() {
+        List<Integer> shipCoordinates = dragAndDrop.getShipCoordinates();
+        List<Integer> shipTemp = new ArrayList<Integer>(shipCoordinates);
+
+        
+
+        return shipTemp;
+    }
+
+
+    // once user confirms they're done with placement this gets called and determines where on the grid the ships will go
+    private static List<Integer> placeShips() {
+        List<Integer> shipCoordinates = dragAndDrop.getShipCoordinates();
+        List<Integer> shipTemp = new ArrayList<Integer>(shipCoordinates);
+
+            int ship1RowStart = (shipCoordinates.get(0)/100);
+            System.out.print("SHIP 1 ROW B4: ");
+            System.out.println(shipCoordinates.get(0));
+            if (ship1RowStart <= 0) {
+                ship1RowStart = 0;
+                shipTemp.set(0, 0);
+            } else {
+                ship1RowStart += 1;
+                shipTemp.set(0, ship1RowStart);
+            }
+            
+            int ship1ColStart = (shipCoordinates.get(1)/100);
+            System.out.print("SHIP 1 COL B4: ");
+            System.out.println(shipCoordinates.get(1));
+            if(ship1ColStart <= 0) {
+                ship1ColStart = 0;
+                shipTemp.set(1, 0);
+            } else {
+                ship1ColStart += 1;
+                shipTemp.set(1, ship1ColStart);
+            }
+            //int ship1RowSpan = (shipCoordinates.get(2)/100);
+            //int ship1ColSpan = (shipCoordinates.get(3)/100);
+
+            int ship2RowStart = (shipCoordinates.get(2)/100);
+            System.out.print("SHIP 2 ROW B4: ");
+            System.out.println(shipCoordinates.get(2));
+            if (ship2RowStart <= 0) {
+                ship2RowStart = 0;
+                shipTemp.set(2, 0);
+            } else {
+                ship2RowStart += 1;
+                shipTemp.set(2, ship2RowStart);
+            }
+
+            int ship2ColStart = (shipCoordinates.get(3)/100);
+            System.out.print("SHIP 2 COL B4: ");
+            System.out.println(shipCoordinates.get(3));
+            if (ship2ColStart <= 0) {
+                ship2ColStart = 0;
+                shipTemp.set(3, 0);
+            } else {
+                ship2ColStart += 1;
+                shipTemp.set(3, ship2ColStart);
+            }
+            //int ship2RowSpan = (shipCoordinates.get(6)/100);
+            //int ship2ColSpan = (shipCoordinates.get(7)/100);
+
+            int ship3RowStart = (shipCoordinates.get(4)/100);
+            System.out.print("SHIP 3 ROWB4: ");
+            System.out.println(shipCoordinates.get(4));
+            if (ship3RowStart <= 0) {
+                ship3RowStart = 0;
+                shipTemp.set(4, 0);
+            } else {
+                ship3RowStart += 1;
+                shipTemp.set(4, ship3RowStart);
+            }
+
+            int ship3ColStart = (shipCoordinates.get(5)/100);
+            System.out.print("SHIP 3 COL B4: ");
+            System.out.println(shipCoordinates.get(5));
+            if (ship3ColStart <= 0) {
+                ship3ColStart = 0;
+                shipTemp.set(5, 0);
+            } else {
+                ship3ColStart = (ship3ColStart + 1);
+                shipTemp.set(5, ship3ColStart);
+            }
+            //int ship3RowSpan = (shipCoordinates.get(10)/100);
+            //int ship3ColSpan = (shipCoordinates.get(11)/100);
+
+            System.out.print("SHIP 4 ROW B4: ");
+            System.out.println(shipCoordinates.get(6));
+            int ship4RowStart = (shipCoordinates.get(6)/100);
+            if (ship4RowStart <= 0) {
+                ship4RowStart = 0;
+                shipTemp.set(6, 0);
+            } else {
+                ship4RowStart += 1;
+                shipTemp.set(6, ship4RowStart);
+            }
+            
+            int ship4ColStart = (shipCoordinates.get(7)/100);
+            System.out.print("SHIP 4 COL B4: ");
+            System.out.println(shipCoordinates.get(7));
+            if (ship4ColStart <= 0) {
+                ship4ColStart = 0;
+                shipTemp.set(7, 0);
+            } else {
+                ship4ColStart += 1;
+                shipTemp.set(7, ship4ColStart);
+            }
+            //int ship4RowSpan = (shipCoordinates.get(14)/100);
+            //int ship4ColSpan = (shipCoordinates.get(15)/100);
+
+            int ship5RowStart = (shipCoordinates.get(8)/100);
+            System.out.print("SHIP 5 ROW B4: ");
+            System.out.println(shipCoordinates.get(8));
+            if (ship5RowStart <= 0) {
+                ship5RowStart = 0;
+                shipTemp.set(8, 0);
+            } else {
+                ship5RowStart += 1;
+                shipTemp.set(8, ship5RowStart);
+            }
+            int ship5ColStart = (shipCoordinates.get(9)/100);
+            System.out.print("SHIP 5 COL B4: ");
+            System.out.println(shipCoordinates.get(9));
+            if (ship5ColStart <= 0) {
+                ship5ColStart = 0;
+                shipTemp.set(9, 0);
+            } else {
+                ship5ColStart += 1;
+                shipTemp.set(9, ship5ColStart);
+            }
+            //int ship5RowSpan = (shipCoordinates.get(18)/100);
+            //int ship5ColSpan = (shipCoordinates.get(19)/100);
+            return shipTemp;
+        }
+        
+
+
+
+
 
     private static JPanel initializeEnemyBoardPanel() {
         JPanel boardPanel = new JPanel();
--- MyPanel.java
+++ MyPanel.java
@@ -1,6 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -15,9 +16,10 @@ class DraggableImage {
     ImageIcon vertImageIcon;
     Point imageUpperLeft, prevPoint;
     Boolean horizontal;
+    int length;
 
 
-    DraggableImage(String imagePath, String imagePathH, Point initialLocation) {
+    DraggableImage(String imagePath, String imagePathH, Point initialLocation, int length) {
         try {
             File file = new File(imagePath);
             BufferedImage bufferedImage = ImageIO.read(file);
@@ -28,6 +30,7 @@ class DraggableImage {
             BufferedImage bufferedImage2 = ImageIO.read(file2);
             horizImageIcon = new ImageIcon(bufferedImage2);
             horizontal = false;
+            this.length = length;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,7 +39,9 @@ class DraggableImage {
         return new Rectangle((int) imageUpperLeft.getX(), (int) imageUpperLeft.getY(),
                 getImageIcon().getIconWidth(), getImageIcon().getIconHeight());
     }
-
+    int getlen(){
+        return length;
+    }
     void rotate(){
         horizontal = !horizontal;
     }
@@ -47,6 +52,7 @@ class DraggableImage {
         }
         return vertImageIcon;
     }
+
 }
 
 
@@ -57,11 +63,11 @@ class MyPanel extends JPanel{
 
     MyPanel(){
             imageList = new Vector<>();
-            imageList.add(new DraggableImage("./ship1.png","./ship1h.png", new Point(100, 100))); // Add first image
-            imageList.add(new DraggableImage("./ship2.png","./ship2h.png", new Point(150, 100))); // Add first image
-            imageList.add(new DraggableImage("./ship3.png","./ship3h.png", new Point(200, 100))); // Add first image
-            imageList.add(new DraggableImage("./ship4.png","./ship4h.png", new Point(250, 100))); // Add first image
-            imageList.add(new DraggableImage("./ship5.png","./ship5h.png", new Point(300, 100))); // Add second image
+            imageList.add(new DraggableImage("./ship1.png","./ship1h.png", new Point(100, 100),5)); // Add first image
+            imageList.add(new DraggableImage("./ship2.png","./ship2h.png", new Point(150, 100),4)); // Add first image
+            imageList.add(new DraggableImage("./ship3.png","./ship3h.png", new Point(200, 100),3)); // Add first image
+            imageList.add(new DraggableImage("./ship4.png","./ship4h.png", new Point(250, 100),3)); // Add first image
+            imageList.add(new DraggableImage("./ship5.png","./ship5h.png", new Point(300, 100),2)); // Add second image
             ClickListener clickListener = new ClickListener();
             this.addMouseListener(clickListener);
             DragListener dragListener = new DragListener();
@@ -118,16 +124,33 @@ class MyPanel extends JPanel{
 
     private class DragListener extends MouseMotionAdapter{
         public void mouseDragged(MouseEvent event) {
-                if (selectedImage!=null){
-                    Point currPoint = event.getPoint();
-                    int dx = (int) (currPoint.getX() - selectedImage.prevPoint.getX());
-                    int dy = (int) (currPoint.getY() - selectedImage.prevPoint.getY());
-                    selectedImage.imageUpperLeft.translate(dx, dy);
-                    selectedImage.prevPoint = currPoint;
-                    repaint();
-                }
+            if (selectedImage!=null){
+                Point currPoint = event.getPoint();
+                int dx = (int) (currPoint.getX() - selectedImage.prevPoint.getX());
+                int dy = (int) (currPoint.getY() - selectedImage.prevPoint.getY());
+                selectedImage.imageUpperLeft.translate(dx, dy);
+                selectedImage.prevPoint = currPoint;
+                repaint();
             }
         }
     }
+
+    public List<Double> getShipCoordinates() {
+        List<Double> coords = new ArrayList<>();
+        for (int i = 0; i < imageList.size(); i++) {
+            DraggableImage image = imageList.get(i);
+            Point location = image.imageUpperLeft;
+            coords.add((int) location.getY());
+            coords.add((int) location.getX());
+            //coords.add(image.getImageIcon().getIconWidth());
+            //coords.add(image.getImageIcon().getIconHeight());
+        }
+        return coords;
+    }
+
+    public boolean getHorizontal(int index) {
+        return imageList.get(index).horizontal;
+    }
+}
 
 
